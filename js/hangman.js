a01f739d68a8b2684cc3cd6685cc5bca634e8de3
andreas Nesheim <nesheim123@hotmail.com>
2023-01-15 18:18:02 +0100

    Updated UML

--- js/hangman.js
+++ js/hangman.js
@@ -2,37 +2,23 @@ $(document).ready(function () {
     scoreBoard();
 });
 
-class user {
-    constructor(
-        name,
-        score,
-        maxGuesses,
-        yourGuesses = [],
-        palyerSucessWords = []
-    ){
-        this.name = name;
-        this.playerScore = score;
-        this.maxGuesses = maxGuesses;
-        this.yourGuesses = yourGuesses;
-        this.palyerSucessWords = palyerSucessWords
-    }
+var userName = null;
+var userScore = null;
+var theWordToGuess = '';
+var secretWord = null;
+var difficulty = null;
+const guessedLetters = [];
+const succesLetters = [];
 
-}
+let maxGuesses = parseInt(theWordToGuess.length * 1.5);
 
 function hangMeNot() {
-    // function Variables
-    let theWord = 'Guess me';
-    let guessWordLength = theWord.length;
-    let guessWordSmalCaps = theWord.toLocaleLowerCase();
-    let maxGuesses = parseInt(guessWordLength * 1.5);
-
-    // User variables
-    const yourGuesses = [];
-    const palyerSucessWords = [];
-    const userName = '';
-    let userScore = 0;
-    let dificulty = 'easy';
-
+    
+    try {
+        selectAWord()
+    } catch (e) {
+        selectAWord()
+    }
     // Game Loop
 
     /* Ask for input 
@@ -57,41 +43,63 @@ function hangMeNot() {
         alert(`To the gallows! \n"${yourGuesses[yourGuesses.length - 1]}" is not part of the word `)
     }
 
-    /* Display the current status
-        - Display the guessed letters
-        - display the word whit unlocked letters
-        - display the gallow progress
-   */
-    // Guessd Letters
-    for (let i = 0; i < yourGuesses.length; i++) {
-        $('#yourGuesses').append(`<p class="btn btn-outline-primary">${yourGuesses[i].toLocaleUpperCase()}</p>`);
-    };
-    // Unlocked Word
-    const tempWord = []
-    let unlocked = '';
-    for (let i = 0; i < palyerSucessWords.length; i++) {
-        for (let j = 0; j < guessWordLength ; j++) {
-            if (palyerSucessWords[i].toLowerCase() == theWord[j].toLowerCase()) {
-                tempWord[j] = theWord[j];
-            }
-            else{
-                tempWord[j] ='';
-            }
-        }
-    }
-    for (let i = 0; i < tempWord.length; i++){
-        unlocked.concat(tempWord[i]);
+
+
+    //Game end
+    if (!maxGuesses || secretword === theWordToGuess) {
+        gameEnd();
     }
     $('#theGuessedWord').html(unlocked);
 
 };
 
+function gameEnd() {
+    console.log('Game Over!')
+
+    scoreBoard();
+    
+}
+
 // update the palyer scoreboard
 function scoreBoard() {
     $('#scoreBoardTable tbody').html('This is a message from the JS');
+    console.log(`usr: ${userName} score: ${getScore()} word:${theWordToGuess} progress: ${secretWord}`);
 };
 
-// generic function to update the DOM
-function populateDom(value, tag = null, id = null, type = null) {
+function retunProgress () {
+    /* Display the current status
+            - Display the guessed letters
+            - display the word whit unlocked letters
+            - display the gallow progress
+       */
+    // Guessed Letters
+   
+    // Unlocked Word
+    const tempWord = []
+    let unlocked = '';
 
+    for (let i = 0; i < palyerSucessWords.length; i++) {
+        for (let j = 0; j < guessWordLength; j++) {
+            if (palyerSucessWords[i].toLowerCase() == theWord[j].toLowerCase()) {
+                tempWord[j] = theWord[j];
+            }
+            else {
+                tempWord[j] = '';
+            }
+        }
+    }
+    for (let i = 0; i < tempWord.length; i++) {
+        unlocked.concat(tempWord[i]);
+    }
 }
+
+function selectAWord() {
+    const list = ['a','b','c'];
+
+    theWordToGuess = list[Math.round(Math.random() * (list.length))];
+}
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+*/
